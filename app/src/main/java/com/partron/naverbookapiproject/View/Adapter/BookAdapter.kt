9f0e15e1b3ff31d85ab4cb15f20e8bf988098922
user <sonny0204@gmail.com>
2023-05-28 20:22:56 +0900

    data binding 개발

--- app/src/main/java/com/partron/naverbookapiproject/View/Adapter/BookAdapter.kt
+++ app/src/main/java/com/partron/naverbookapiproject/View/Adapter/BookAdapter.kt
@@ -6,11 +6,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.partron.naverbookapiproject.Https.Data.Book
+import com.partron.naverbookapiproject.Utill.OnItemClickListener
 import com.partron.naverbookapiproject.databinding.AdapterBookBinding
 
 class BookAdapter (private val context : Context) : RecyclerView.Adapter<BookAdapter.ViewHolder>() {
     var data = ArrayList<Book>()
-
+    private var onClickListener : OnItemClickListener ?= null
     inner class  ViewHolder( val binding : AdapterBookBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -19,14 +20,25 @@ class BookAdapter (private val context : Context) : RecyclerView.Adapter<BookAda
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.txtTitle.text = data[position].title
+        holder.binding.argTitle = data[position].title
+        holder.binding.argLink = data[position].link
+//        holder.binding.txtTitle.text = data[position].title
         Glide.with(context)
             .load(data[position].image)
             .into(holder.binding.imgBook)
+        //recyclerview Click Listener
+        holder.binding.txtLink.setOnClickListener {
+            onClickListener!!.onClickListener(data[position].link)
+        }
     }
 
     override fun getItemCount(): Int {
        return data.size
+    }
+
+    //interface 정의
+    fun setOnClickListener(clickListener : OnItemClickListener){
+        this.onClickListener = clickListener
     }
 
 }
