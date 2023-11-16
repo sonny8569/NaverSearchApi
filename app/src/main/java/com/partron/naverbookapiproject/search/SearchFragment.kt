d38f6812ee34b098338b6560003603d8f25353c0
sonny8569 <sonny132@naver.com>
2023-11-16 23:14:30 +0900

    기존 Resource 함수 사용 -> 자체 sealed class 사용

--- app/src/main/java/com/partron/naverbookapiproject/search/SearchFragment.kt
+++ app/src/main/java/com/partron/naverbookapiproject/search/SearchFragment.kt
@@ -1,4 +1,4 @@
-package com.partron.naverbookapiproject.View
+package com.partron.naverbookapiproject.search
 
 import android.content.Intent
 import android.net.Uri
@@ -10,20 +10,15 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.partron.naverbookapiproject.BuildConfig
 import com.partron.naverbookapiproject.R
-import com.partron.naverbookapiproject.Respository.RepositoryImpl
-import com.partron.naverbookapiproject.Utill.OnItemClickListener
-import com.partron.naverbookapiproject.Utill.Resource
-import com.partron.naverbookapiproject.View.Adapter.BookAdapter
-import com.partron.naverbookapiproject.ViewModel.Factory.SearchViewModelFactory
-import com.partron.naverbookapiproject.ViewModel.SearchViewModel
+import com.partron.naverbookapiproject.utill.OnItemClickListener
+import com.partron.naverbookapiproject.search.viewModel.SearchViewModel
 import com.partron.naverbookapiproject.databinding.FragmentSearchBinding
+import com.partron.naverbookapiproject.search.adapter.BookAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -32,18 +27,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
     private val TAG =  "SearchFragment"
-
-    private var _binding : FragmentSearchBinding ? = null
-    private val binding get() = _binding!!
-
-//    private var _viewModel : SearchViewModel ? = null
-//    private val viewModel get() = _viewModel!!
-    private val viewModel : SearchViewModel by activityViewModels<SearchViewModel>()
+    private lateinit var binding : FragmentSearchBinding
     private lateinit var adapter : BookAdapter
+    private val viewModel : SearchViewModel by activityViewModels()
 
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        _binding = FragmentSearchBinding.inflate(inflater , container , false )
-//        initViewModel()
+        binding = FragmentSearchBinding.inflate(inflater , container , false )
         return binding.root
     }
 
@@ -51,24 +40,12 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         init()
         addListener()
+        observer()
     }
-
-//    /**
-//     * viewModel init
-//     */
-//    private fun initViewModel(){
-//        val factory = SearchViewModelFactory(RepositoryImpl())
-//        _viewModel = ViewModelProvider(this , factory)[SearchViewModel :: class.java]
-//    }
-
     private fun init(){
         val layoutManger : RecyclerView.LayoutManager = LinearLayoutManager(requireContext())
         binding.recyclerviewBook.layoutManager = layoutManger
         adapter = BookAdapter(requireContext())
-
-//        if(viewModel.bookSearchLiveData.value?.data?.size != null) {
-//            adapter.data = viewModel.bookSearchLiveData.value?.data!!
-//        }
     }
 
     private fun addListener(){
@@ -80,30 +57,6 @@ class SearchFragment : Fragment() {
             }
             viewModel.requestBookApi(BuildConfig.CLIENT_ID , BuildConfig.CLIENT_PASSWORD , data)
         }
-
-
-        viewModel.bookSearchLiveData.observe(viewLifecycleOwner , Observer {
-            when(it.status){
-                Resource.Status.LOADING ->{
-                    Log.d(TAG , "Loading for search Book")
-                }
-                Resource.Status.SUCCESS ->{
-                    Log.d(TAG , "Success to get Book")
-                    val result = it.data!!
-                    adapter.data = result
-                    binding.recyclerviewBook.adapter = adapter
-                }
-                Resource.Status.ERROR ->{
-                    Log.e(TAG , "Error to get Book")
-                    Toast.makeText(requireContext() , getString(R.string.msg_check_network) , Toast.LENGTH_SHORT).show()
-                }
-                Resource.Status.FaIL ->{
-                    Log.d(TAG , "Fail to get Book")
-                    Toast.makeText(requireContext() , it.message , Toast.LENGTH_SHORT).show()
-                }
-            }
-        })
-
         adapter.setOnClickListener(object: OnItemClickListener{
             override fun onClickListener(data: String) {
                 if(data == ""){
@@ -115,6 +68,26 @@ class SearchFragment : Fragment() {
             }
         })
     }
+    private fun observer(){
+        viewModel.bookSearchLiveData.observe(viewLifecycleOwner , Observer {
+            when(it){
+                is SearchViewModel.SearchViewResult.Loading ->{
+                    Log.d(TAG , it.message)
+                }
+                is SearchViewModel.SearchViewResult.Book->{
+                    Log.d(TAG , "Success to get Book")
+                    val result = it.data
+                    adapter.data = result.items
+                    binding.recyclerviewBook.adapter = adapter
+                }
+                is SearchViewModel.SearchViewResult.ERROR->{
+                    Log.e(TAG , "Error to get Book")
+                    Toast.makeText(requireContext() , getString(R.string.msg_check_network) , Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+    }
     private fun goToBookPage(url : String){
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         startActivity(intent)
@@ -122,8 +95,6 @@ class SearchFragment : Fragment() {
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.e(TAG, "The Search View is Destory")
-        _binding = null
-//        _viewModel = null
+        Log.e(TAG, "The Search View is Destroy")
     }
 }
